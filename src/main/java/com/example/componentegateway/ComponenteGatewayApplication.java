bc02344e1fa274c2ca1aa16820d5f2823e0a164f
Luan Felipe Marmentini <luanf2003@gmail.com>
2024-12-15 14:11:25 -0300

    Initial commit

---
+++ src/main/java/com/example/componentegateway/ComponenteGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.componentegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponenteGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponenteGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/componentegateway/ComponenteGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.componentegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponenteGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
