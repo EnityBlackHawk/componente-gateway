68ba781c33effb07ec396b86f0da7d21109ba341
Luan Felipe Marmentini <luanf2003@gmail.com>
2024-12-15 14:11:36 -0300

    Add files

--- src/main/java/com/example/componentegateway/ComponenteGatewayApplication.java
+++ src/main/java/com/example/componentegateway/ComponenteGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ComponenteGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComponenteGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ComponenteGatewayApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/componentegateway/ComponenteGatewayApplicationTests.java
+++ src/test/java/com/example/componentegateway/ComponenteGatewayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ComponenteGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
